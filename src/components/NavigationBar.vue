fc182de2b232521d917b4c44d0f3bdc7b3d3f00c
Brandon Giampa <30915668+brandongiampa@users.noreply.github.com>
2022-11-25 22:30:55 -0800

    create navbar

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,58 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark text-light">
+        <div class="container">
+            <router-link class="nav-link" to="/">
+              <span class="branding">BG</span>
+            </router-link>
+            <button 
+                class="navbar-toggler" 
+                type="button" data-bs-toggle="collapse" 
+                data-bs-target="#navbarNavAltMarkup" 
+                aria-controls="navbarNavAltMarkup" 
+                aria-expanded="false" 
+                aria-label="Toggle navigation"
+            >
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <router-link class="nav-link mt-2" to="/portfolio">Portfolio</router-link>
+                    <router-link class="nav-link mt-2" to="/about">About</router-link>
+                    <a class="nav-link brands" href="#"><font-awesome-icon icon="fa-brands fa-github" /></a>
+                    <a class="nav-link brands" href="#"><font-awesome-icon icon="fa-brands fa-linkedin" /></a>
+                </div>
+            </div>
+          </div>
+    </nav>
+</template>
+
+<style lang="scss">
+nav {
+  padding: 30px;
+  .navbar-nav {
+    margin-left: auto;
+  }
+  .branding {
+    font-family: Fascinate, serif;
+    font-size: 30px;
+  }
+  .brands {
+    font-size: 30px;
+    margin-top: -4px;
+  }
+  a {
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #efefef;;
+    }
+  }
+  img {
+    max-height: 60px;
+  }
+  @media only screen and (max-width: 400px) {
+    img {
+      max-height: 50px;
+    }
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,22 @@
+import "./app.scss"
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
 
-createApp(App).use(store).use(router).mount('#app')
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
+/* add icons to the library */
+library.add(faGithub)
+library.add(faLinkedin)
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).use(store).use(router).mount('#app')
+
+import "bootstrap/dist/js/bootstrap.js"
