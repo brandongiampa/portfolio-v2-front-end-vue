d7d7225a38d451488d2b9a025bcef57b212c3774
Brandon Giampa <30915668+brandongiampa@users.noreply.github.com>
2022-11-29 08:46:09 -0800

    add dummy works module to store

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import { createStore } from 'vuex'
 import aboutTextModule from './modules/AboutTextModule.js'
+import PortfolioWorksModule from './modules/PortfolioWorksModule.js'
 
 export default createStore({
   state: {
@@ -11,6 +12,7 @@ export default createStore({
   actions: {
   },
   modules: {
-    aboutTextModule
+    aboutTextModule,
+    PortfolioWorksModule
   }
 })
