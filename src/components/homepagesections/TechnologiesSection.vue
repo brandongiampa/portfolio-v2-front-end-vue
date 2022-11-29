b9bdd193b85f5ac80f4e7512611fec6eec404bbe
Brandon Giampa <30915668+brandongiampa@users.noreply.github.com>
2022-11-28 19:57:27 -0800

    add homepage technologies section

---
+++ src/components/homepagesections/TechnologiesSection.vue
@@ -0,0 +1,61 @@
+<template>
+    <section class="p-5">
+        <div class="container">
+            <h1 class="underlined mb-5">Technologies</h1>
+            <br/>
+            <div class="pt-4" id="technologies-bg">
+                <div class="row">
+                    <div class="col-3 d-flex align-items-center justify-content-center p-5" v-for="(technology, index) of technologies" :key="'technology-link-'+(index+1)">
+                        <a :href="'./portfolio?technology='+technology.name">
+                            <img :src="technology.imgUrl" :alt="technology.name" />
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            technologies: [
+                {
+                    name: 'HTML5',
+                    imgUrl: 'https://imgur.com/hzgllqZ.png'
+                },
+                {
+                    name: 'CSS3',
+                    imgUrl: 'https://imgur.com/tFctgth.png'
+                },
+                {
+                    name: 'Javascript',
+                    imgUrl: 'https://imgur.com/MVGRrVh.png'
+                },
+                {
+                    name: 'PHP',
+                    imgUrl: 'https://imgur.com/WQDVLq4.png'
+                },
+                {
+                    name: 'WordPress',
+                    imgUrl: 'https://imgur.com/hy2bfkZ.png'
+                },
+                {
+                    name: 'Vue',
+                    imgUrl: 'https://imgur.com/rq7uYft.png'
+                }
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    #technologies-bg {
+        background-color: #e9ecef;
+    }
+    img {
+        height: 120px;
+    }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,7 @@
     <hero-showcase></hero-showcase>
     <about-section></about-section>
     <works-section></works-section>
+    <technologies-section></technologies-section>
   </div>
 </template>
 
@@ -10,13 +11,15 @@
 import HeroShowcase from '@/components/HeroShowcase.vue'
 import AboutSection from '@/components/homepagesections/AboutSection.vue'
 import WorksSection from '@/components/homepagesections/WorksSection.vue'
+import TechnologiesSection from '@/components/homepagesections/TechnologiesSection.vue'
 
 export default {
   name: 'HomeView',
   components: {
     HeroShowcase,
     AboutSection,
-    WorksSection
+    WorksSection,
+    TechnologiesSection
   }
 }
 </script>
