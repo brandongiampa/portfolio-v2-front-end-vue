c120056ed0193119471d5bcbfbb01330b1b1b496
Brandon Giampa <30915668+brandongiampa@users.noreply.github.com>
2022-12-11 20:44:15 -0800

    Add splash sscreen for loading page with custom-colored progress bar.

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,34 @@
   <router-view/>
   <site-footer></site-footer>
   <testimonial-modal />
+  <splash-screen v-if="isLoading" :progress-value="progressValue" />
 </template>
 
 <script>
   import NavigationBar from './components/NavigationBar.vue'
   import SiteFooter from './components/SiteFooter.vue'
+  import SplashScreen from './components/SplashScreen.vue'
   import TestimonialModal from './components/modals/TestimonialModal.vue'
   export default {
     components: {
       NavigationBar,
       SiteFooter,
-      TestimonialModal
+      TestimonialModal,
+      SplashScreen
     },
     async created() {
       await this.$store.dispatch('setWorks', this.$store.getters.works)
+      this.progressValue = 50
       await this.$store.dispatch('setTestimonials')
+      this.progressValue = 100
       //do something to unhide UI
+      this.isLoading = false
+    },
+    data() {
+      return {
+        progressValue: 0,
+        isLoading: true
+      }
     }
   }
 </script>
---
+++ src/components/SplashScreen.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="splashscreen">
+        <div>
+            <code class="text-light loading"><i>Loading brandongiampa.com...</i></code>
+            <br/>
+            <progress max="100" :value="progressValue"></progress>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            progressValue: {
+                type: Number
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    .splashscreen {
+        position: fixed;
+        width: 100%;
+        height: 100vh;
+        left: 0;
+        top: 0;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        background-color: black;
+    }
+</style>
