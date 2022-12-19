19f3fcc39c45ada23ec8e1bef18684a99a43551c
Brandon Giampa <30915668+brandongiampa@users.noreply.github.com>
2022-12-18 19:37:34 -0800

    Create H1 custom component so gsap can animate underline.

---
+++ src/components/reusable/UnderlinedH1.vue
@@ -0,0 +1,33 @@
+<template>
+    <h1 class="mt-2 mb-5 underlined">
+        <span 
+            v-for="(letter, index) of title" 
+            :key="`h1-span-${index}`"
+            :class="buildLetterSpanClasses(letter)"
+        >
+            {{letter}}
+        </span>
+        <div class="underline"/>
+    </h1>
+</template>
+
+<script>
+    export default {
+        props: {
+            title: {
+                type: String,
+                required: true
+            }
+        },
+        methods: {
+            buildLetterSpanClasses(letter) {
+                const display = letter !== " " ? 'd-inline-block ' : ""
+                return display + 'name-letter-span'
+            }
+        },
+        mounted() {
+            console.log("H1 mounted.")
+        }
+    }
+</script>
+
