c8357ca38d18b6934c38bf80eaf46e5d0c15d96a
Brandon Giampa <30915668+brandongiampa@users.noreply.github.com>
2023-01-02 13:35:59 -0800

    Remove placeholder testimonials and use ones from API.

--- src/components/homepagesections/TestimonialsSection.vue
+++ src/components/homepagesections/TestimonialsSection.vue
@@ -8,7 +8,7 @@
                 :key="'homepage-testimonial-'+(index+1)"
                 :id="'testimonial-'+(index+1)"
             >
-                <p style="color: #343a40;" class="mb-1 testimonial-meta"><b>{{testimonial.author}}</b> from <a :href="testimonial.companyUrl">{{testimonial.company}}</a> says:</p>
+                <p style="color: #343a40;" class="mb-1 testimonial-meta"><b>{{testimonial.author}}</b> from <a :href="testimonial.company_url">{{testimonial.company}}</a> says:</p>
                 <p class="mt-0 text-black testimonial-text">{{testimonial.text}}</p>
             </div>
             <button 
@@ -25,22 +25,9 @@
 
 <script>
 export default {
-    data() {
-        return {
-            testimonials: [
-                {
-                    author: 'Ari Fine',
-                    company: 'roofgenius.com',
-                    companyUrl: 'https://roofgenius.com',
-                    text: "Brandon was awesome to work with! He's super knowledgeable, was responsive and easy to communicate with, and took a really proactive approach to helping me solve the issues I was having with my Wordpress blog. He went above and beyond on this project - Brandon pointed out and took care of a few issues that I hadn't even noticed and that were outside the scope of our arrangement, which I really appreciated. I'm planning to enlist Brandon's help with future projects, and would absolutely recommend working with him."
-                },
-                {
-                    author: 'Gluten Free Beer Brian',
-                    company: 'GFB Supply',
-                    companyUrl: 'https://gfbsupply.com',
-                    text: "Brandon Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui porro distinctio itaque eos voluptatum pariatur fuga fugiat, nemo nam dicta excepturi adipisci possimus cupiditate laudantium? Voluptatum ad officia ipsum, incidunt est dolores, nihil praesentium at quod harum numquam recusandae! Itaque nostrum voluptatem ex sed!"
-                },
-            ]
+    computed: {
+        testimonials: function() {
+            return this.$store.getters.testimonials
         }
     }
 }
